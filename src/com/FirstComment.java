a0041951e503ce25180b1c542e0156f6c3b2b2fe
mangesh687 <mangeshpakhale2016@gmail.com>
2023-06-10 13:09:32 +0530

    commit first

---
+++ src/com/FirstComment.java
@@ -0,0 +1,11 @@
+package com;
+
+public class FirstComment {
+
+	public static void main(String[] args) {
+   System.out.println("test_1");
+   System.out.println("test_2");
+   System.out.println("test_3");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mange
+ *
+ */
+module PCW {
+}
